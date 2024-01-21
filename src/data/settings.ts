52867dbcb301c2a4fc2ae823d674d153b3e0392c
Ali Raza <57584928+aliraza5490@users.noreply.github.com>
2024-01-21 16:54:55 +0500

    update info

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
-  site: 'https://cannonball.littlesticks.dev',
-  name: 'Little Sticks',
+  site: 'https://aliwebdev.tech',
+  name: 'Ali Web Developer',
   title: 'Cannonball by Little Sticks | A splash page template',
   description: 'This is a simple splash page template built with Astro by Little Sticks',
 }
