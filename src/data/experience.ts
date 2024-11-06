404289521771250320ba4c044c14fe886f83b934
Ali Raza <2020cs08@stu.mnsuet.edu.pk>
2024-11-06 22:10:19 +0500

    update title

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -3,7 +3,7 @@ import type { Props as ExperienceProp } from '../components/experience/props';
 export const EXPERIENCE: ExperienceProp[] = [
   {
     dates: 'November 2020 — Present',
-    title: 'Web Developer',
+    title: 'MERN Stack & Next.js Developer',
     company: 'Fiverr',
     companyUrl: 'https://www.fiverr.com/users/aliansari78692',
     description:
